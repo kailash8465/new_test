f387821a690c2f659a14b5393184b94571a32242
kailash8465 <60359651+kailash8465@users.noreply.github.com>
2023-05-19 09:00:17 +0530

    Update automate_git1.py

--- automate_git1.py
+++ automate_git1.py
@@ -1,21 +1,21 @@
 import requests
 import os
 def create_branch(access_token, owner, repo_name, base_branch, new_branch):
-    branch_url = f"https://api.github.com/repos/{owner}/{repo_name}/git/refs"
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-        "Accept": "application/vnd.github.v3+json"
-    }
-    payload = {
-        "ref": f"refs/heads/{new_branch}",
-        "sha": f"refs/heads/{base_branch}"
-    }
-    response = requests.post(branch_url, json=payload, headers=headers)
-    if response.status_code == 201:
-        print(f"Branch '{new_branch}' created successfully!")
-    else:
-        print(f"Failed to create branch '{new_branch}'")
-        print(f"Response: {response.status_code} - {response.text}")
+  branch_url = f"https://api.github.com/repos/{owner}/{repo_name}/git/refs"
+  headers = {
+      "Authorization": f"Bearer {access_token}",
+      "Accept": "application/vnd.github.v3+json"
+  }
+  payload = {
+      "ref": f"refs/heads/{new_branch}",
+      "sha": f"refs/heads/{base_branch}"
+  }
+  response = requests.post(branch_url, json=payload, headers=headers)
+  if response.status_code == 201:
+      print(f"Branch '{new_branch}' created successfully!")
+  else:
+      print(f"Failed to create branch '{new_branch}'")
+      print(f"Response: {response.status_code} - {response.text}")
 
 
 # Replace YOUR_PAT with your personal access token
