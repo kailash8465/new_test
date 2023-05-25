49f300c633545d1e7553ddbc7284ac376c0e0c8a
kailash8465 <60359651+kailash8465@users.noreply.github.com>
2023-05-25 23:31:07 +0530

    Update protect.py

--- protect.py
+++ protect.py
@@ -1,30 +1,11 @@
-import requests
 import os
+from github import Github
 
-def protect_github_branch(repo_owner, repo_name, branch_name, access_token):
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-        "Accept": "application/vnd.github.luke-cage-preview+json"  # Required for working with branch protection API
-    }
-
-    # Get branch protection settings
-    response = requests.get(f"https://api.github.com/repos/{repo_owner}/{repo_name}/branches/{branch_name}/protection", headers=headers)
-    response.raise_for_status()
-    branch_protection = response.json()
-
-    # Update branch protection settings
-    if branch_name == "develop":
-        branch_protection["required_pull_request_reviews"] = {"dismiss_stale_reviews": True, "require_code_owner_reviews": True}
-        branch_protection["required_status_checks"] = None
-    else:
-        branch_protection["required_pull_request_reviews"] = None
-        branch_protection["required_status_checks"] = {"strict": True, "contexts": []}
-    
-    branch_protection["enforce_admins"] = False
-    branch_protection["restrictions"] = None
-
-    response = requests.put(f"https://api.github.com/repos/{repo_owner}/{repo_name}/branches/{branch_name}/protection", headers=headers, json=branch_protection)
-    response.raise_for_status()
+def enable_branch_protection(repo_owner, repo_name, branch_name, access_token):
+    g = Github(access_token)
+    repo = g.get_repo(f"{repo_owner}/{repo_name}")
+    branch = repo.get_branch(branch_name)
+    branch.edit_protection(required_approving_review_count=1, dismiss_stale_reviews=True)
 
 # Example usage
 repo_owner = "kailash8465"
@@ -32,4 +13,4 @@ repo_name = "final_test"
 branch_name = "develop"
 github_access_token = os.environ.get('token')
 
-protect_github_branch(repo_owner, repo_name, branch_name, github_access_token)
+enable_branch_protection(repo_owner, repo_name, branch_name, github_access_token)
