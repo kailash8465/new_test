037b57775c24a76b7a835d982a03d6809422a1df
kailash8465 <60359651+kailash8465@users.noreply.github.com>
2023-05-29 22:43:03 +0530

    Update environ1.py

--- environ1.py
+++ environ1.py
@@ -1,27 +1,38 @@
 import requests
 import os
 api_token = os.environ.get('token')
-
-headers = {
-    "Accept": "application/vnd.github+json",
-    "Authorization": f"Bearer {api_token}",
-    "X-GitHub-Api-Version": "2022-11-28"
-}
-
-data = {
-    "wait_timer": 30,
-    "reviewers": [
-        {"type": "User", "id": 60359651}
-    ],
-    "deployment_branch_policy": {
-        "protected_branches": False,
-        "custom_branch_policies": True
+def create_github_environment(repository, environment_name):
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {api_token}",
+        "X-GitHub-Api-Version": "2022-11-28"
     }
-}
 
-url = "https://api.github.com/repos/kailash8465/final_test/environments/stg-deploy"
+    data = {
+#         "wait_timer": 30,
+        "reviewers": [
+            {"type": "User", "id": 60359651}
+#             {"type": "Team", "id": 1}
+        ]
+#         "deployment_branch_policy": {
+#             "protected_branches": False,
+#             "custom_branch_policies": True
+#         }
+    }
 
-response = requests.put(url, headers=headers, json=data)
+    url = f"https://api.github.com/repos/{repository}/environments/{environment_name}"
 
-print("Status code:", response.status_code)
-print("Response:", response.json())
+    response = requests.put(url, headers=headers, json=data)
+
+    if response.status_code == 200:
+        print("Environment created/updated successfully!")
+    else:
+        print("Failed to create/update environment. Error:", response.text)
+
+# Example usage
+repository = "kailash8465/final_test"
+environment_name = ['stg-deploy','prod_deploy']
+
+for i in environment_name:
+    create_github_environment(repository, i)
+
