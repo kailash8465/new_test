ad34adf9843c850da4342c0969cbe28c64411c40
kailash8465 <60359651+kailash8465@users.noreply.github.com>
2023-05-29 21:40:46 +0530

    Update environ.py

--- environ.py
+++ environ.py
@@ -3,31 +3,33 @@ import requests
 repo_owner = "kailash8465"
 repo_name = "final_test"
 api_token = os.environ.get('token')
-def create_environment_with_reviewers(environment):
-    # Define the API endpoint for creating an environment
-    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/environments"
 
-    # Set up the required headers including authentication
+def create_environment_with_reviewers(ENVIRONMENT_NAME):
+    url = "https://api.github.com/repos/OWNER/REPO/environments/ENVIRONMENT_NAME"
     headers = {
-        "Accept": "application/vnd.github.v3+json",
-        "Authorization": f"Bearer {api_token}"
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {api_token}",
+        "X-GitHub-Api-Version": "2022-11-28"
     }
-
-    # Set up the required data for creating the environment
+    
     data = {
-        "name": environment,
-        "required reviewers": [
-            "c360-admins",
-        ]
+        "wait_timer": 30,
+        "reviewers": [
+            {"type": "User", "id": 'Bhargavi-stanleymldl'},
+#             {"type": "Team", "id": 1}
+        ],
+        "deployment_branch_policy": {
+            "protected_branches": False,
+            "custom_branch_policies": True
+        }
     }
-
-    # Send a POST request to create the environment
-    response = requests.post(url, headers=headers, json=data)
-
-    # Check the response status code
+    
+    response = requests.put(url, headers=headers, json=data)
+    
     if response.status_code == 201:
         print("Environment created successfully.")
     else:
         print(f"Failed to create environment. Status code: {response.status_code}")
         print(response.json())
-create_environment_with_reviewers('stg-repo-deploy')
+
+create_environment_with_reviewers('stg-deploy')
