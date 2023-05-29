8dbe88b6fca1ee1a95a129924574a8adcb066c9e
kailash8465 <60359651+kailash8465@users.noreply.github.com>
2023-05-29 22:18:02 +0530

    Create environ1.py

---
+++ environ1.py
@@ -0,0 +1,28 @@
+import requests
+import json
+import os
+
+def create_github_environment(repository, environment_name):
+    api_url = f"https://api.github.com/repos/{repository}/environments"
+    headers = {
+        "Accept": "application/vnd.github.v3+json",
+        "Authorization": "Bearer os.environ.get('token')"
+    }
+    payload = {
+        "name": environment_name,
+        "auto_merge": False,
+        "short_name": environment_name[:20],
+        "production": False
+    }
+
+    response = requests.post(api_url, headers=headers, data=json.dumps(payload))
+    if response.status_code == 201:
+        print("Environment created successfully!")
+    else:
+        print("Failed to create environment. Error:", response.text)
+
+# Example usage
+repository = "kailash8465/final_test"
+environment_name = "my-new-environment"
+
+create_github_environment(repository, environment_name)
