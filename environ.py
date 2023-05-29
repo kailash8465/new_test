0a78c33c941c65679c7a73c4c01658e12a8d9fd9
kailash8465 <60359651+kailash8465@users.noreply.github.com>
2023-05-29 21:51:08 +0530

    Update environ.py

--- environ.py
+++ environ.py
@@ -4,28 +4,32 @@ repo_owner = "kailash8465"
 repo_name = "final_test"
 api_token = os.environ.get('token')
 
+import requests
+
 def create_environment_with_reviewers(ENVIRONMENT_NAME):
-    url = "https://api.github.com/repos/OWNER/REPO/environments/ENVIRONMENT_NAME"
+    url = "https://api.github.com/repos/OWNER/REPO/deployments/ENVIRONMENT_NAME/statuses"
+#     api_token = "YOUR-TOKEN"
+    user_id = 'Bhargavi-stanleymldl'  # Replace with the actual team ID
+
     headers = {
         "Accept": "application/vnd.github+json",
         "Authorization": f"Bearer {api_token}",
         "X-GitHub-Api-Version": "2022-11-28"
     }
-    
+
     data = {
-        "wait_timer": 30,
-        "reviewers": [
-            {"type": "User", "id": 'Bhargavi-stanleymldl'},
-#             {"type": "Team", "id": 1}
-        ],
-        "deployment_branch_policy": {
-            "protected_branches": False,
-            "custom_branch_policies": True
-        }
+        "state": "pending",
+#         "environment_url": "https://example.com",
+        "required_reviewers": [
+            {
+                "type": "Team",
+                "id": user_id
+            }
+        ]
     }
-    
+
     response = requests.post(url, headers=headers, json=data)
-    
+
     if response.status_code == 201:
         print("Environment created successfully.")
     else:
@@ -33,3 +37,4 @@ def create_environment_with_reviewers(ENVIRONMENT_NAME):
         print(response.json())
 
 create_environment_with_reviewers('stg-deploy')
+
