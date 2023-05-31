189930340c86b3d03ff1d736597f643ae1a22f0a
kailash8465 <60359651+kailash8465@users.noreply.github.com>
2023-05-31 07:48:46 +0530

    Update create_repo.py

--- create_repo.py
+++ create_repo.py
@@ -1,27 +1,28 @@
 import os
 import requests
 
-api_token=os.environ.get('token')
-owner='kailash8465'
+api_token = os.environ.get('token')
+owner = 'kailash8465'
+
 def create_private_repo_with_auto_init(repository_name):
     headers = {
-    "Authorization": f"token {api_token}",
-    "Accept": "application/vnd.github.v3+json"
-}
+        "Authorization": f"token {api_token}",
+        "Accept": "application/vnd.github.v3+json"
+    }
 
-data = {
-    "name": {repository_name},
-    "auto_init": True,
-    'private': True,
-}
+    data = {
+        "name": repository_name,
+        "auto_init": True,
+        "private": True
+    }
 
-create_repo_url = f"https://api.github.com/user/{owner}/repos"
-response = requests.post(create_repo_url, headers=headers, json=data)
+    create_repo_url = f"https://api.github.com/user/repos"
+    response = requests.post(create_repo_url, headers=headers, json=data)
 
-if response.status_code == 201:
-    print("New repository created successfully!")
-else:
-    print("Failed to create repository:", response.json()["message"])
+    if response.status_code == 201:
+        print("New repository created successfully!")
+    else:
+        print("Failed to create repository:", response.json()["message"])
 
 # Replace 'YOUR_GITHUB_TOKEN' with your actual GitHub token
 # Replace 'your-username' with the desired owner or organization name
