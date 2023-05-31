70702c11b44d11258b25cf7a5269581ae52cf1f3
kailash8465 <60359651+kailash8465@users.noreply.github.com>
2023-05-31 07:55:17 +0530

    Update create_repo.py

--- create_repo.py
+++ create_repo.py
@@ -16,7 +16,7 @@ def create_private_repo_with_auto_init(repository_name):
         "private": True
     }
 
-    create_repo_url = f"https://api.github.com/user/repos/{owner}"
+    create_repo_url = f"https://api.github.com/user/{owner}/repos"
     response = requests.post(create_repo_url, headers=headers, json=data)
 
     if response.status_code == 201:
